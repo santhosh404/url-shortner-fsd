98248232816f48e306e26a2c049a0d6edaf65d9c
santhosh annamalai <santhoshmathi2002@gmail.com>
2024-06-11 11:34:43 +0530

    commit

--- front-end/src/services/UserServices.js
+++ front-end/src/services/UserServices.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const BASE_URL = "https://url-shortner-fsd.onrender.com/api/v1";
-// const BASE_URL = "http://localhost:4000/api/v1";
 
 
 export const getUser = async () => {
