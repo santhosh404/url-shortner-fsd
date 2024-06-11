dc106205602c3c4eddfc623822769a0b98251de1
santhosh annamalai <santhoshmathi2002@gmail.com>
2024-06-11 11:06:56 +0530

    base_url changes

--- front-end/src/services/UserServices.js
+++ front-end/src/services/UserServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = "https://url-shortner-fsd.onrender.com/api/v1";
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://url-shortner-fsd.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:4000/api/v1";
 
 
 export const getUser = async () => {
